a8a020f356a656aea90facc43ab8ed6608175404
Jennifer Chow <jennego@gmail.com>
2022-05-13 22:37:23 -0700

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Easy Equestrian Log</h1>
+      </div>
+
+      <ul>
+        <li>Mobile first</li>
+        <li>Easy logging with a few taps/clicks</li>
+        <li>Set horse profiles</li>
+        <li>View data in different ways</li>
+        <li>Add images</li>
+        <li>Add videos (external host?)</li>
+        <li>Easy export?</li>
+      </ul>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
